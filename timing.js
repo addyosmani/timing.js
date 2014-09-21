63844c9a146f840900e670f24aa1d457c94fd30c
Addy Osmani <addyosmani@gmail.com>
2014-09-21 13:39:42 +0100

    Add additonal measurements

--- timing.js
+++ timing.js
@@ -19,6 +19,7 @@
                 }
             }
             
+            // Time to first paint
             if (api.firstPaint === null) {
                 // All times are relative times to the start time within the
                 // same objects
@@ -38,6 +39,29 @@
                     api.firstPaint = window.performance.timing.navigationStart;
                 }
             }
+            
+            // Total time from start to load
+            api.loadTime = timing.loadEventEnd - timing.navigationStart;
+            // Time spent constructing the DOM tree
+            api.domReadyTime = timing.domComplete - timing.domInteractive;
+            // Time consumed prepaing the new page
+            api.readyStart = timing.fetchStart - timing.navigationStart;
+            // Time spent during redirection
+            api.redirectTime = timing.redirectEnd  - timing.redirectStart;
+            // AppCache
+            api.appcacheTime = timing.domainLookupStart  - timing.fetchStart;
+            // Time spent unloading documents
+            api.unloadEventTime = timing.unloadEventEnd - timing.unloadEventStart;
+            // DNS query time
+            api.lookupDomainTime = timing.domainLookupEnd - timing.domainLookupStart;
+            // TCP connection time
+            api.connectTime = timing.connectEnd - timing.connectStart;
+            // Time spent during the request
+            api.requestTime = timing.responseEnd - timing.requestStart;
+            // Request to completion of the DOM loading
+            api.initDomTreeTime = timing.domInteractive - timing.responseEnd;
+            // Load event time
+            api.loadEventTime = timing.loadEventEnd - timing.loadEventStart;
         }
         
         return api;
