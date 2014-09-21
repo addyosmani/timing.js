db86fafe715d21aa925d531675d7bb4ca83536a8
Addy Osmani <addyosmani@gmail.com>
2014-09-21 14:46:41 +0100

    Clean-up the API.

--- timing.js
+++ timing.js
@@ -1,66 +1,68 @@
-(function() {
+(function(window) {
     'use strict';
     /**
-     * Cross-browser Performance Timing measurements
-     * Targeted at evergreen browsers.
+     * User Timing API helpers
+     * timing.getTimes();
      **/
-    function getMeasurements() {
-        var performance = window.performance || window.webkitPerformance || window.msPerformance || window.mozPerformance;
-        var timing = performance.timing;
-        var api = {};
+    window.timing = window.timing || {
+        getTimes: function() {
+            var performance = window.performance || window.webkitPerformance || window.msPerformance || window.mozPerformance;
+            var timing = performance.timing;
+            var api = {};
 
-        if (timing) {
-            for (var k in timing) {
-                if (timing.hasOwnProperty(k)) {
-                    api[k] = timing[k];
+            if (timing) {
+                for (var k in timing) {
+                    if (timing.hasOwnProperty(k)) {
+                        api[k] = timing[k];
+                    }
                 }
+
+                // Time to first paint
+                if (api.firstPaint === null) {
+                    // All times are relative times to the start time within the
+                    // same objects
+
+                    // Chrome
+                    if (!!window.chrome && window.chrome.loadTimes) {
+                        // Convert to ms
+                        api.firstPaint = window.chrome.loadTimes().firstPaintTime * 1000;
+                    }
+                    // IE
+                    else if (typeof api.msFirstPaint === 'number') {
+                        api.firstPaint = api.msFirstPaint;
+                    }
+                    // Firefox
+                    // This will use the first times after MozAfterPaint fires
+                    else if (window.performance.timing.navigationStart && typeof InstallTrigger !== 'undefined') {
+                        api.firstPaint = window.performance.timing.navigationStart;
+                    }
+                }
+
+                // Total time from start to load
+                api.loadTime = timing.loadEventEnd - timing.navigationStart;
+                // Time spent constructing the DOM tree
+                api.domReadyTime = timing.domComplete - timing.domInteractive;
+                // Time consumed prepaing the new page
+                api.readyStart = timing.fetchStart - timing.navigationStart;
+                // Time spent during redirection
+                api.redirectTime = timing.redirectEnd - timing.redirectStart;
+                // AppCache
+                api.appcacheTime = timing.domainLookupStart - timing.fetchStart;
+                // Time spent unloading documents
+                api.unloadEventTime = timing.unloadEventEnd - timing.unloadEventStart;
+                // DNS query time
+                api.lookupDomainTime = timing.domainLookupEnd - timing.domainLookupStart;
+                // TCP connection time
+                api.connectTime = timing.connectEnd - timing.connectStart;
+                // Time spent during the request
+                api.requestTime = timing.responseEnd - timing.requestStart;
+                // Request to completion of the DOM loading
+                api.initDomTreeTime = timing.domInteractive - timing.responseEnd;
+                // Load event time
+                api.loadEventTime = timing.loadEventEnd - timing.loadEventStart;
             }
 
-            // Time to first paint
-            if (api.firstPaint === null) {
-                // All times are relative times to the start time within the
-                // same objects
-
-                // Chrome
-                if (!!window.chrome && window.chrome.loadTimes) {
-                    // Convert to ms
-                    api.firstPaint = window.chrome.loadTimes().firstPaintTime * 1000;
-                }
-                // IE
-                else if (typeof api.msFirstPaint === 'number') {
-                    api.firstPaint = api.msFirstPaint;
-                }
-                // Firefox
-                // This will use the first times after MozAfterPaint fires
-                else if (window.performance.timing.navigationStart && typeof InstallTrigger !== 'undefined') {
-                    api.firstPaint = window.performance.timing.navigationStart;
-                }
-            }
-
-            // Total time from start to load
-            api.loadTime = timing.loadEventEnd - timing.navigationStart;
-            // Time spent constructing the DOM tree
-            api.domReadyTime = timing.domComplete - timing.domInteractive;
-            // Time consumed prepaing the new page
-            api.readyStart = timing.fetchStart - timing.navigationStart;
-            // Time spent during redirection
-            api.redirectTime = timing.redirectEnd - timing.redirectStart;
-            // AppCache
-            api.appcacheTime = timing.domainLookupStart - timing.fetchStart;
-            // Time spent unloading documents
-            api.unloadEventTime = timing.unloadEventEnd - timing.unloadEventStart;
-            // DNS query time
-            api.lookupDomainTime = timing.domainLookupEnd - timing.domainLookupStart;
-            // TCP connection time
-            api.connectTime = timing.connectEnd - timing.connectStart;
-            // Time spent during the request
-            api.requestTime = timing.responseEnd - timing.requestStart;
-            // Request to completion of the DOM loading
-            api.initDomTreeTime = timing.domInteractive - timing.responseEnd;
-            // Load event time
-            api.loadEventTime = timing.loadEventEnd - timing.loadEventStart;
+            return api;
         }
-
-        return api;
-    }
-});
+    };
+})(this);
